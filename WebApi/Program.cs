eab687ea8a1343aa6ef52e6fbf7d5213889bb1d8
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-10 22:43:44 +0530

    configuration Separent layers with DependencyInjection

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,9 +1,18 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
 
 var app = builder.Build();
 
